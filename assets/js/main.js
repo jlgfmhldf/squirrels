ae17c53fb07f47fa63ff7682540987549052b11f
jlgfmhldf <jlgfmhldf@gmail.com@>
2014-12-10 19:32:50 +0300

    new squirells

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.content').mixItUp({
+       
+           animation: {
+                effects: 'fade rotateY(-180deg)',
+                duration: 1000
+         }
+
+    });
+});
